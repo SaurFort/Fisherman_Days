4b6dfbad3d698f3375cca3bb85a70719716a1198
Hakitox1004 <nino@createlier.org>
2025-10-07 12:46:44 +0200

    filet.py

--- modules/filet.py
+++ modules/filet.py
@@ -2,10 +2,11 @@ class Filet:
     def __init__ (self):
         self.niveau = 1
     
-    def ModifDrop ():
+    def modif_taux (self):
         if self.niveau == 1:
             return 0.0
         elif self.niveau == 2:
             return 0.03
         else:
             return 0.10
+
