e2b6ac2eb43aa9ca048ee26ff940ef5245f173f0
Hakitox1004 <nino@createlier.org>
2025-10-07 12:44:34 +0200

    Add files via upload

---
+++ modules/filet.py
@@ -0,0 +1,11 @@
+class Filet:
+    def __init__ (self):
+        self.niveau = 1
+    
+    def ModifDrop ():
+        if self.niveau == 1:
+            return 0.0
+        elif self.niveau == 2:
+            return 0.03
+        else:
+            return 0.10
