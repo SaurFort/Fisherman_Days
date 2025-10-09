0e5dee1ed04cbc8bc80cfc82dbc650eed13bf9d5
SaurFort <tou.nathan@gmail.com>
2025-10-09 09:37:52 +0200

    Changement type bourse : int -> float

--- modules/bourse.py
+++ modules/bourse.py
@@ -1,15 +1,15 @@
 class Bourse:
     def __init__(self) -> None:
-        self.__bourse = 1
+        self.__bourse = 1.0
 
-    def recuperer(self) -> int:
+    def recuperer(self) -> float:
         return self.__bourse
 
-    def ajouter(self, somme: int) -> None:
-        self.__bourse += somme
+    def ajouter(self, somme: float) -> None:
+        self.__bourse += round(somme, 2)
 
-    def retirer(self, somme: int) -> None:
-        self.__bourse -= somme
+    def retirer(self, somme: float) -> None:
+        self.__bourse -= round(somme, 2)
 
     def __str__(self) -> str:
         return f"Argent: {self.__bourse}"
