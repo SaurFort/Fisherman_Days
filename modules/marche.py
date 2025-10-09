449c34184201bdca8504dd83d950dcf77029cda0
Hakitox1004 <nino@createlier.org>
2025-10-09 10:01:50 +0200

    achat objet

--- modules/marche.py
+++ modules/marche.py
@@ -15,6 +15,37 @@ class Marche:
         self.prix_merlin = round(self.prix_merlin * 1.1, 2)
         self.prix_fugu = round(self.prix_fugu * 1.1, 2)
 
+    def boutique (self,bourse,glaciere,filet,radar):
+        prix_glaciere = 50*(self.glaciere.niveau+1)**2+50*(self.glaciere.niveau+1)-100
+        if glaciere.niveau == 4:
+            prix_glaciere = 'MAX'
+            
+            
+        prix_filet = 50*(self.filet.niveau+1)**3-50*(self.filet.niveau+1)
+        if filet.niveau == 3:
+            prix_filet = 'MAX'
+            
+
+        prix_radar = 650
+        if radar.niveau == 1:
+            prix_radar = 'MAX'
+
+        choix=int(input(f"-VOUS ÊTES DANS LA BOUTIQUE-\n1. Glacière [{prix_glaciere}]\n2. Filet [{prix_filet}]\n3. Radar [{prix_radar}]"))
+        if choix == 1:
+            if glaciere.niveau < 4 and bourse.recuperer >= prix_glaciere:
+                bourse.retirer(prix_glaciere)
+                glaciere.niveau += 1
+                
+        if choix == 2:
+            if filet.niveau < 3 and bourse.recuperer >= prix_filet:
+                bourse.retirer(prix_filet)
+                filet.niveau += 1
+            
+        if choix == 3:
+            if glaciere.niveau < 1 and bourse.recupere >= prix_radar:
+                bourse.retirer(prix_radar)
+                radar.niveau += 1
+
     def vente(self, glaciere: Glaciere) -> int:
         self.__inflation()
         argent = 0
