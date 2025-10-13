fb5ec4e24ff33518ddb3a05866c4c19ce8f66ec4
Hakitox1004 <nino@createlier.org>
2025-10-13 17:57:18 +0200

    modif fin

--- modules/joueur.py
+++ modules/joueur.py
@@ -70,6 +70,9 @@ class Joueur :
         print("Vous venez d'acheter ce bibelot très joli, en l'achetant vous avez éprouver une énorme joie et partie de la boutique sans demander votre reste.")
         print(f"En arrivant chez vous, vous vous rendez compte que sous le bibelot il y a écrit une phrase : \"Vous venez de vous faire arnaquez de {prix_bibelot}, merci de m'avoir payer mon voyage au bahamas !\".")
         print("Après avoir lu tout ça vous ressentez une soudaine tristesse puisque vous avez eu beaucoup de mal pour l'acheter.")
+        print("vous avvez désormais un nouvel élément de décors qui vous rappelle toute votre aventure")
+        print("Fin.)
+
 
 
 
