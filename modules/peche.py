17b406eb88928846e6f07eaab08c9b23edd8e378
SaurFort <tou.nathan@gmail.com>
2025-10-16 09:44:15 +0200

    Correction bug pêche (merci Nino ...)

--- modules/peche.py
+++ modules/peche.py
@@ -34,28 +34,23 @@ class Peche:
             nom_poisson = "Thon |💠💠💠|"
         elif poisson.categorie == "Merlin":
             nom_poisson = "Merlin |✨|"
-        elif poisson.categorie == "Fugu":
+        else:
             nom_poisson = "Fugu |💀|"
-        
-            choix = 0
-            if poisson.categorie == "Fugu":
-                choix = input(f"Ho! Vous avez attrapé un {nom_poisson}, vos trois dernières captures ont été perdus,\n impossible de retirer le Fugu de la glacière.\n1|⭕| Suivant")
-                if choix == 1 or choix != 1:
-                    for i in range(3):
-                        self.glaciere.relacher_poisson()
-                self.glaciere.stocker_poisson(poisson)
-                print("|✅|Vous avez stocké ce poisson.")
-                print("Votre stock:\n", self.glaciere)
-            
-            if poisson.categorie == "Maquereau" or "Aiglefin" or "Thon" or "Merlin": #ou bien (elif p.c != "Fugu")
-                choix = input(f"Vous avez attrapé un {nom_poisson}, voulez-vous le garder ?\n1. Oui\n2. Non\n\n -> ")
-                if choix == 1:
-                    if self.glaciere.place_disponible() == 0:
-                        self.glaciere.relacher_poisson()
 
+        if poisson.categorie == "Fugu":
+            input(f"Ho! Vous avez attrapé un {nom_poisson}, vos trois dernières captures ont été perdus,\n impossible de retirer le Fugu de la glacière.\n|⭕| Suivant\n\n")
+            for i in range(3):
+                self.glaciere.relacher_poisson()
+            self.glaciere.stocker_poisson(poisson)
+            print("|✅|Vous avez stocké ce poisson.")
+        else:
+            choix = input(f"Vous avez attrapé un {nom_poisson}, voulez-vous le garder ?\n1. Oui\n2. Non\n\n -> ")
+            if choix == "1":
+                if self.glaciere.place_disponible() == 0:
+                    self.glaciere.relacher_poisson()
                 self.glaciere.stocker_poisson(poisson)
                 print("|✅|Vous avez stocké ce poisson.")
-                else:
-                    print("|❌|Vous avez relâché ce poisson.")
+            else:
+                print("|❌|Vous avez relâché ce poisson.")
 
         print("Votre stock:\n", self.glaciere)
