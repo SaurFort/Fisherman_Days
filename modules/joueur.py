7f83cfd2e402ded1a2daa11c642f07de67e7ab24
Hakitox1004 <nino@createlier.org>
2025-10-27 11:24:08 +0100

    correction fautes + message de fin :)

--- modules/joueur.py
+++ modules/joueur.py
@@ -32,11 +32,11 @@ class Joueur :
             if choix == 6:
                 self.rentrer_prematurer()
 
-        print("Vous n'avez plus de fioul, vous êtes obligés de rentrer.")
+        print("Vous n'avez plus de fioul, vous êtes obligé de rentrer.")
         self.rentrer_prematurer()
 
     def affichage2(self):
-        choix = int(input("-VOUS ÊTES AU MARCHE- ?\n1|💰| Boutique\n2|⛔| Retourner en session\n\n"))
+        choix = int(input("-VOUS ÊTES AU MARCHÉ- ?\n1|💰| Boutique\n2|⛔| Retourner en session\n\n"))
 
         if choix == 1:
             self.marche.boutique(self)
@@ -67,13 +67,15 @@ class Joueur :
         self.radar.afficher(self.filet.taux(self.glaciere.place_disponible()))
 
     def fin(self, prix_bibelot):
-        print("Vous venez d'acheter ce bibelot très joli, en l'achetant vous avez éprouver une énorme joie et êtes partie de la boutique sans demander votre reste.")
-        print("En arrivant chez vous, vous vous rendez compte que sous le bibelot il y a écrit une phrase :")
-        print(f"\"Vous venez de vous faire arnaquez de {prix_bibelot}💲, merci de m'avoir payer mon voyage au bahamas !\"")
-        print("Après avoir lu tout ça vous ressentez une soudaine tristesse puisque vous avez eu beaucoup de mal pour l'acheter.")
-        print("Vous avez désormais un nouvel élément de décors qui vous rappelle toute votre aventure")
+        print("Vous venez d'acheter ce très joli bibelot, en l'achetant vous avez éprouver une énorme joie et êtes parti de la boutique sans demander votre reste.")
+        print("En arrivant chez vous, vous vous apercevez qu'une phrase est écrite sous l'objet :")
+        print(f"\"Vous venez de vous faire arnaquez {prix_bibelot}💲, merci de m'avoir payer mon voyage au bahamas ! AH ! AH !\"")
+        print("Après avoir lu ceci vous ressentez une soudaine tristesse du au mal que vous vous êtes donné pour l'obtenir.")
         print("Fin.")
+        print(" ")
+        print("Merci d'avoir joué, MoonCore")
         sys.exit()
+
 
 
 
