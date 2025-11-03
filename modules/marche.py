270c6fb60e834f456112a4a7f890857dc4d6399c
SaurFort <tou.nathan@gmail.com>
2025-11-03 13:15:13 +0100

    Rééquilibrage

--- modules/marche.py
+++ modules/marche.py
@@ -15,7 +15,7 @@ class Marche:
         self.prix_thon = round(self.prix_thon * 1.01, 2)
         self.prix_merlin = round(self.prix_merlin * 1.01, 2)
         self.prix_fugu = round(self.prix_fugu * 1.01, 2)
-        self.prix_bibelot = round(self.prix_bibelot * 1.1, 0)
+        self.prix_bibelot = round(self.prix_bibelot * 1.20, 0)
 
     def boutique (self,joueur):
         joueur.voir_bourse()
@@ -69,19 +69,19 @@ class Marche:
         for poisson in compte:
             if poisson == "Aiglefin":
                 argent += compte[poisson] * self.prix_aiglefin
-                self.prix_aiglefin = round((self.prix_aiglefin * 0.98) * compte[poisson], 2)
+                self.prix_aiglefin = round((self.prix_aiglefin * 0.97) * compte[poisson], 2)
             elif poisson == "Thon":
                 argent += compte[poisson] * self.prix_thon
-                self.prix_thon = round((self.prix_thon * 0.98) * compte[poisson], 2)
+                self.prix_thon = round((self.prix_thon * 0.97) * compte[poisson], 2)
             elif poisson == "Merlin":
                 argent += compte[poisson] * self.prix_merlin
-                self.prix_merlin = round((self.prix_merlin * 0.98) * compte[poisson], 2)
+                self.prix_merlin = round((self.prix_merlin * 0.97) * compte[poisson], 2)
             elif poisson == "Fugu":
                 argent += compte[poisson] * self.prix_fugu
-                self.prix_fugu = round((self.prix_fugu * 0.98) * compte[poisson], 2)
+                self.prix_fugu = round((self.prix_fugu * 0.97) * compte[poisson], 2)
             else:
                 argent += compte[poisson] * self.prix_maquereau
-                self.prix_maquereau = round((self.prix_maquereau * 0.98) * compte[poisson], 2)
+                self.prix_maquereau = round((self.prix_maquereau * 0.97) * compte[poisson], 2)
 
         glaciere.vider()
         return argent
