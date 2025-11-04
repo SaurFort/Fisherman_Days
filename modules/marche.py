9624bd324838953014ca70209daeee5c755a5b72
Hakitox1004 <nino@createlier.org>
2025-11-04 07:20:22 +0100

    Update marche.py

--- modules/marche.py
+++ modules/marche.py
@@ -32,7 +32,7 @@ class Marche:
         if joueur.radar.niveau == 1:
             prix_radar = 'MAX'
 
-        choix = Validateur.choix(f"-VOUS ÊTES DANS LE MARCHÉ-\n1. Glacière+ [{prix_glaciere}💲]\n2. Filet+ [{prix_filet}💲]\n3. Radar [{prix_radar}💲] !\n4. Joli bibelot [{self.prix_bibelot}💲] -1 seul exemplaire-\n5.|⛔|Retour au port \n\n _", ["1","2","3","4","5","fugu&ships"])
+        choix = Validateur.choix(f"-VOUS ÊTES DANS LE MARCHÉ-\n1. Glacière+ [{prix_glaciere}💲]\n2. Filet+ [{prix_filet}💲]\n3. Radar [{prix_radar}💲] !\n4. Joli bibelot [{self.prix_bibelot}💲] ⭐ \n5.|⛔|Retour au port \n\n _", ["1","2","3","4","5","fugu&ships"])
         # Condition de debug
         if choix == "fugu&ships":
             joueur.bourse.ajouter(1000000)
@@ -42,19 +42,19 @@ class Marche:
             if joueur.glaciere.niveau < 4 and joueur.bourse.recuperer() >= prix_glaciere:
                 joueur.bourse.retirer(prix_glaciere)
                 joueur.glaciere.niveau += 1
-                print("✅ Votre Glacière a été amélioré avec succès ! ✅")
+                print("✅ Votre Glacière a été amélioré avec succès ! ✅\n")
                 
         if choix == "2":
             if joueur.filet.niveau < 3 and joueur.bourse.recuperer() >= prix_filet:
                 joueur.bourse.retirer(prix_filet)
                 joueur.filet.niveau += 1
-                print("✅ Votre Filet a été amélioré avec succès ! ✅")
+                print("✅ Votre Filet a été amélioré avec succès ! ✅\n")
             
         if choix == "3":
             if joueur.radar.niveau < 1 and joueur.bourse.recuperer() >= prix_radar:
                 joueur.bourse.retirer(prix_radar)
                 joueur.radar.niveau += 1
-                print("✅ Votre Radar a été acquis avec succès ! ✅")
+                print("✅ Votre Radar a été acquis avec succès ! ✅\n")
 
         if choix == "4":
             if joueur.bourse.recuperer() >= self.prix_bibelot:
