5ec04380869ce8d271809a0325244e6c7641cc66
SaurFort <tou.nathan@gmail.com>
2025-11-06 08:57:36 +0100

    Commentaire jeu

--- jeu.py
+++ jeu.py
@@ -1,5 +1,6 @@
 from modules.joueur import Joueur
 
+# On oblige le joueur à jouer au jeu :)
 print("Bienvenue dans )-Fisherman Days-(.\nDans ce petit jeu vous suivrez la vie d'un misérable pêcheur qui essaie tant bien que mal de gagner sa vie afin d'acheter un objet bizarre.")
 choix = input("Êtes-vous prêt ? (Oui/oui)\n")
 
@@ -8,5 +9,5 @@ if choix.lower() == "oui" or choix.lower() == "o":
 else:
     print("De toute façon vous n'avez pas le choix !")
 
-joueur = Joueur()
-joueur.affichage()
+# On initialise le joueur et on appelle aussitôt la méthode affichage qui lance le jeu.
+Joueur().affichage()
