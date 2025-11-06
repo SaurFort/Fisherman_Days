1c7965348d05612d0728ed8c515791beda3762f5
SaurFort <tou.nathan@gmail.com>
2025-11-06 08:29:41 +0100

    Commentaire poisson

--- modules/poisson.py
+++ modules/poisson.py
@@ -1,3 +1,10 @@
 class Poisson:
     def __init__(self, categorie: str):
+        """
+        Initialise un poisson avec une catégorie donnée.
+        La catégorie prend généralement les valeurs : `maquereau`, `aiglefin`, `thon` ou `merlin`.
+
+        Args:
+            categorie (str): La catégorie du poisson.
+        """
         self.categorie = categorie
