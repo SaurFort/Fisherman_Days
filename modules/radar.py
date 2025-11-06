858232023c9f17463b7df7952d989d93dcaed39d
Haki1004 <nino@createlier.org>
2025-11-06 09:11:30 +0100

    Update radar.py

--- modules/radar.py
+++ modules/radar.py
@@ -1,20 +1,11 @@
 
 class Radar:
     def __init__ (self):
-        self.niveau = 0
+        self.niveau = 0 #défini le niveau de base du radar (0)
     
     def afficher (self,proba):
         if self.niveau == 0:
-            return "|#####|"
+            return "|#####|" # si le joueur n'a pas encore acheté le radar, on lui retourne du bruit blanc 
         if self.niveau >= 1:
-            return f"|💠: {proba[0]}% |💠💠: {proba[1]}% |💠💠💠: {proba[2]}% |✨: {proba[3]}% |💀: {proba[4]}%|"
-
-
-
-
-
-
-
-
-
-
+            return f"|💠: {proba[0]}% |💠💠: {proba[1]}% |💠💠💠: {proba[2]}% |✨: {proba[3]}% |💀: {proba[4]}%|" 
+            # si le joueur a bien acheté le radar (niveau 1), on lui retourne les probabilités en temps réel avec les améliorations du filet
