cbaf6ca6b10381d69432392a8f9cbe25031ba75f
SaurFort <tou.nathan@gmail.com>
2025-11-06 08:52:09 +0100

    Commentaire validateur

--- modules/validator2000.py
+++ modules/validator2000.py
@@ -1,5 +1,16 @@
 class Validateur:
     def choix(question: str, whitelist: list[str]) -> bool:
+        """
+        Demande une réponse de l'utilisateur jusqu'à ce que ce soit une valeur enregistrée dans la liste blanche.
+        Cela permet de s'assurer d'obtenir une réponse valide, qui ne cassera pas le programme.
+
+        Args:
+            question (str): La question à poser à l'utilisateur.
+            whitelist (list[str]): La liste des réponses acceptée.
+
+        Returns:
+            str: La réponse de l'utilisateur parmi les éléments de la liste des réponses acceptables.
+        """
         reponse = ""
         while reponse not in whitelist:
             reponse = input(question)
